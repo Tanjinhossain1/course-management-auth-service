1251c6b0cb9caf11abffea7402c66474605b40c0
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-06-07 22:41:59 +0600

    make zod validation for user

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,22 +1,49 @@
-import mongoose, { CastError } from 'mongoose'
-import { IGenericErrorMessage } from '../types/errors'
-import { IErrorReturnResponseType } from '../types/common'
+import mongoose, { CastError } from 'mongoose';
+import { IGenericErrorMessage } from '../types/errors';
+import { IErrorReturnResponseType } from '../types/common';
+import { ZodError, ZodIssue } from 'zod';
+
+/* ****************************************************************** */
+/* *************   mongoose validation error handler   ************** */
+/* ****************************************************************** */
 
 export const handleValidationError = (
-  err: mongoose.Error.ValidationError
+  error: mongoose.Error.ValidationError
 ): IErrorReturnResponseType => {
-  const errors: IGenericErrorMessage[] = Object.values(err.errors).map(
+  const errors: IGenericErrorMessage[] = Object.values(error.errors).map(
     (errorEl: mongoose.Error.ValidatorError | CastError) => {
       return {
         path: errorEl?.path,
         message: errorEl?.message,
-      }
+      };
     }
-  )
-  const statusCode = 400
+  );
+  const statusCode = 400;
   return {
     statusCode: statusCode,
     errorMessages: errors,
     message: 'Validation Error',
-  }
-}
+  };
+};
+
+/* ****************************************************************** */
+/* ****************  Zod validation error handler  ****************** */
+/* ****************************************************************** */
+
+export const validationZodError = (
+  error: ZodError
+): IErrorReturnResponseType => {
+  const errors: IGenericErrorMessage[] = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+
+  const statusCode = 400;
+  return {
+    statusCode: statusCode,
+    errorMessages: errors,
+    message: 'Validation Error',
+  };
+};
