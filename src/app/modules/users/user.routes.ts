64828472a2192199aeac75303c273a3fe09f3162
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-06-07 10:38:06 +0600

    setup and validate user zod and setup handler and midleware in routes

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { createUserController } from './user.controller'
+import { ValidationRequest } from '../../midlewares/validateRequest'
+import { userSchemaValidation } from './user.validation'
 
 const usersRouter = express.Router()
 
-usersRouter.post('/users-create', createUserController)
+usersRouter.post(
+  '/users-create',
+  ValidationRequest(userSchemaValidation),
+  createUserController
+)
 
 export default usersRouter
