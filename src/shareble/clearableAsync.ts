9626908a3e917931bae1bf2036aa99386b0f9ea4
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-06-09 17:36:33 +0600

    clearable async bug fix

--- src/shareble/clearableAsync.ts
+++ src/shareble/clearableAsync.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response, RequestHandler } from 'express';
 export const clearableAsync = (func: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      func(req, res, next);
+      await func(req, res, next);
     } catch (err) {
       next(err);
     }
